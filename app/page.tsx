6340e64cc2ca9b42bbf4f29b9a91f9285adb0df1
patolord <rodrigo_luizp@hotmail.com>
2023-12-08 18:21:06 -0300

    chore: translate

--- app/page.tsx
+++ app/page.tsx
@@ -16,11 +16,7 @@ export default function Page() {
           <p
             className={`${lusitana.className} text-gray-800 file:text-xl md:text-3xl md:leading-normal`}
           >
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
-            <a href="https://nextjs.org/learn/" className="text-blue-500">
-              Next.js Learn Course
-            </a>
-            , brought to you by Vercel.
+            <strong>Bem-vindo.</strong>
           </p>
           <Link
             href="/login"
