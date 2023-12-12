7b1533d3ca151d8b838d718fcbd7b7949836fa64
patolord <rodrigo_luizp@hotmail.com>
2023-12-12 10:36:56 -0300

    added pwa manifest

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,51 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
-import { Metadata } from 'next';
+import type { Metadata } from 'next';
+
+const APP_NAME = 'PWA App';
+const APP_DEFAULT_TITLE = 'My Awesome PWA App';
+const APP_TITLE_TEMPLATE = '%s - PWA App';
+const APP_DESCRIPTION = 'Best PWA app in the world!';
 
 export const metadata: Metadata = {
+  applicationName: APP_NAME,
   title: {
-    template: '%s | RLP Dashboard',
-    default: 'RLP Dashboard',
+    default: APP_DEFAULT_TITLE,
+    template: APP_TITLE_TEMPLATE,
   },
-  description: 'RLP Dashboard',
-  robots: 'noindex, nofollow',
+  description: APP_DESCRIPTION,
+  manifest: '/manifest.json',
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: 'default',
+    title: APP_DEFAULT_TITLE,
+    // startUpImage: [],
+  },
+  formatDetection: {
+    telephone: false,
+  },
+  openGraph: {
+    type: 'website',
+    siteName: APP_NAME,
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+  twitter: {
+    card: 'summary',
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
   metadataBase: new URL('https://app.rlpeng.com.br'),
+};
+
+export const viewport = {
+  themeColor: '#FFFFFF',
 };
 
 export default function RootLayout({
