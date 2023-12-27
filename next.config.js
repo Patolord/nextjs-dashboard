bb301380ee8567e17621d9952182cbdbeb0d9702
patolord <rodrigo_luizp@hotmail.com>
2023-12-27 19:34:09 -0300

    changed development env config

--- next.config.js
+++ next.config.js
@@ -9,16 +9,15 @@ const withPWA = require('@ducanh2912/next-pwa').default({
   disable: false,
   workboxOptions: {
     disableDevLogs: true,
-    mode: 'production',
   },
 });
 
 const nextConfig = {
-  experimental: {
-    serverActions: {
-      allowedOrigins: [process.env.ALLOWED_ORIGIN, 'localhost:3000'],
-      allowedForwardedHosts: [process.env.ALLOWED_ORIGIN, '127.0.0.1:3000'],
-    },
-  },
+//  experimental: {
+//    serverActions: {
+//      allowedOrigins: [process.env.ALLOWED_ORIGIN, 'localhost:3000'],
+//      allowedForwardedHosts: [process.env.ALLOWED_ORIGIN, '127.0.0.1:3000'],
+//    },
+//  },
 };
 module.exports = withPWA(nextConfig);
