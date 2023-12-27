b11a2cbc302f151d882c1c9bb0651a58f6613e96
patolord <rodrigo_luizp@hotmail.com>
2023-12-27 19:24:07 -0300

    prisma instance fix

--- app/lib/db.ts
+++ app/lib/db.ts
@@ -1,4 +1,24 @@
 import { PrismaClient } from '@prisma/client';
 
-export const prisma = new PrismaClient();
+// Declare a global variable type to extend the NodeJS global type
+declare global {
+  // Prevent TypeScript errors for our custom global variable
+  // by declaring it in the global namespace
+  var prisma: PrismaClient | undefined;
+}
 
+let prisma: PrismaClient;
+
+if (process.env.NODE_ENV === "production") {
+  // In production, always create a new Prisma Client
+  prisma = new PrismaClient();
+} else {
+  // In development, use the global `prisma` instance,
+  // if it doesn't exist, create a new one
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
+}
+
+export { prisma };
